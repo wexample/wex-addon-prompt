678a71c21e8d6d357acf5bb8da25b04cb8d2090b
weeger <romain.weeger@gmail.com>
2023-02-27 07:47:34 +0100

    Add descriptions

--- bash/prompt/choiceGetValue.sh
+++ bash/prompt/choiceGetValue.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+promptChoiceGetValueArgs() {
+  _DESCRIPTION="Get the interactively chosen value by user"
+}
+
 promptChoiceGetValue() {
   wex-exec var/get -n=CHOICE_SELECTED_VALUE
 }
